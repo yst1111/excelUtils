e45f453ae6c849a55fdba295f6b9643732c36e93
ly-yangst <yangst@szlanyou>
2022-05-13 11:49:33 +0800

    utils_git-push-refound

--- src/test/java/com/kundy/excelutils/NewTests.java
+++ src/test/java/com/kundy/excelutils/NewTests.java
@@ -7,7 +7,6 @@ package com.kundy.excelutils;
 public class NewTests {
     public static void main(String[] args) {
         System.out.println("merge");
-        System.out.println("utils_git-push-9:54");
         System.out.println("utils-push-10:10  replace 9:53");
     }
 }
